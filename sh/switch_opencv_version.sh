a768c162758f796c438e3bd0fca229cc0f47bfdd
willard-yuan <yongyuanstu@gmail.com>
2015-06-30 19:36:48 +0800

    witch_opencv_version

---
+++ sh/switch_opencv_version.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+echo /usr/local/opt/opencv/lib/python2.7/site-packages >> /Users/willard/anaconda/lib/python2.7/site-packages/homebrew.pth # choose opencv2
+
+#echo /usr/local/opt/opencv3/lib/python2.7/site-packages >> /Users/willard/anaconda/lib/python2.7/site-packages/opencv3.pth # choose opencv3
