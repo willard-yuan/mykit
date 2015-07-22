e943503f353705d58a51eb0e80138700dcf979d6
willard-yuan <yongyuanstu@gmail.com>
2015-07-22 22:53:11 +0800

    cpu script

---
+++ sh/look_up_cpu_information_in_osx.h
@@ -0,0 +1,8 @@
+o -n "CPU型号:    " 
+sysctl -n machdep.cpu.brand_string
+echo -n "CPU核心数:  " 
+sysctl -n machdep.cpu.core_count
+echo -n "CPU线程数:  "
+sysctl -n machdep.cpu.thread_count
+echo "其它信息："
+system_profiler SPDisplaysDataType SPMemoryDataType SPStorageDataType | grep 'Graphics/Displays:\|Chipset Model:\|VRAM (Total):\|Resolution:\|Memory Slots:\|Size:\|Speed:\|Storage:\|Media Name:\|Medium Type:'
