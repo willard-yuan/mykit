ccfec3ac0b0fd310c655bb597d6f30261d54e8bd
willard-yuan <yongyuanstu@gmail.com>
2015-07-22 22:55:17 +0800

    update

--- sh/look_up_cpu_information_in_osx.h
+++ sh/look_up_cpu_information_in_osx.h
@@ -6,3 +6,4 @@ echo -n "CPU线程数:  "
 sysctl -n machdep.cpu.thread_count
 echo "其它信息："
 system_profiler SPDisplaysDataType SPMemoryDataType SPStorageDataType | grep 'Graphics/Displays:\|Chipset Model:\|VRAM (Total):\|Resolution:\|Memory Slots:\|Size:\|Speed:\|Storage:\|Media Name:\|Medium Type:'
+                                                                                    # sysctl -a | grep ".cpu."
