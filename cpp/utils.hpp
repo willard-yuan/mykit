92eaf7dc52fe0c42774fcae70f9bed78cd0ee35e
Yong Yuan <yongyuanstu@gmail.com>
2017-04-07 21:34:21 +0800

    Update and rename utils.h to utils.hpp

---
+++ cpp/utils.hpp
@@ -0,0 +1,37 @@
+/***************************
+ * My useful functions
+ * Author: yuanyong.name
+ ***************************/
+
+#ifndef utils_hpp
+#define utils_hpp
+
+#include <stdio.h>
+#include <glob.h>
+#include <math.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <numeric>
+
+/***************************
+ * File processing functions
+ ***************************/
+
+std::vector<std::string> split(const std::string &s, char delim);
+std::vector<std::string> glob_vector(const std::string& pattern);
+
+template<class T>
+T base_name(T const & path, T const & delims = "/\\");
+
+template<class T>
+T remove_extension(T const & filename);
+
+
+/***************************
+ * Math processing functions
+ ***************************/
+
+float cosine_similarity(std::vector<float> v1, std::vector<float> v2);
+
+#endif /* utils_hpp */
