8449fce19a0fbe9ba7ba346e6eae7564b14d23e1
willard-yuan <yongyuanstu@gmail.com>
2017-04-12 23:11:56 +0800

    new function

--- cpp/utils.cpp
+++ cpp/utils.cpp
@@ -46,6 +46,22 @@ std::string remove_extension(std::string const & filename){
     return p > 0 && p != std::string::npos ? filename.substr(0, p) : filename;
 }
 
+// Split string by substring
+void SplitString(const std::string& s, std::vector<std::string>& v, const std::string& c){
+    std::string::size_type pos1, pos2;
+    pos2 = s.find(c);
+    pos1 = 0;
+    while(std::string::npos != pos2)
+    {
+        v.push_back(s.substr(pos1, pos2-pos1));
+        
+        pos1 = pos2 + c.size();
+        pos2 = s.find(c, pos1);
+    }
+    if(pos1 != s.length())
+        v.push_back(s.substr(pos1));
+}
+
 
 /***************************
  * Math processing functions
--- cpp/utils.hpp
+++ cpp/utils.hpp
@@ -1,6 +1,6 @@
+
 /***************************
  * My useful functions
- * Author: yuanyong.name
  ***************************/
 
 #ifndef utils_hpp
@@ -23,6 +23,7 @@ std::vector<std::string> split(const std::string &s, char delim);
 std::vector<std::string> glob_vector(const std::string& pattern);
 std::string base_name(std::string const & path, std::string const & delims = "/\\");
 std::string remove_extension(std::string const & filename);
+void SplitString(const std::string& s, std::vector<std::string>& v, const std::string& c);
 
 
 /***************************
